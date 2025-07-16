6036f30af0933c53656ce04d0187d530c8ab434e
jabberjabberjabber <75267439+jabberjabberjabber@users.noreply.github.com>
2025-07-16 09:59:27 -0400

    simple upscale using ffmpeg

--- src/video_3d_pipeline/__init__.py
+++ src/video_3d_pipeline/__init__.py
@@ -4,13 +4,13 @@ __version__ = "0.1.0"
 
 from .align import VideoAligner
 from .depth import HybridStereoDepthExtractor
-from .upscale import GuidedDepthUpscaler
+from .upscale import SimpleDepthUpscaler
 from .utils import get_video_info, extract_audio, verify_video_compatibility
 
 __all__ = [
     "VideoAligner", 
     "HybridStereoDepthExtractor",
-    "GuidedDepthUpscaler",
+    "SimpleDepthUpscaler",
     "get_video_info", 
     "extract_audio", 
     "verify_video_compatibility"
